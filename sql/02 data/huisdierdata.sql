b47a7bda68806d537686d0ca01b6cce649eff4fb
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-05-12 14:35:25 +0200

    a

---
+++ sql/02 data/huisdierdata.sql
@@ -0,0 +1,6 @@
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES (NULL, 'jop', 'jop', 'hond', '11', '2025-05-13');
+
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES (NULL, 'henk', 'jop', 'hond', '11', '2025-05-13'),
+       (NULL, 'henk', 'bob', 'hond', '11', '2025-05-13');
