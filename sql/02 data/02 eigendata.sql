f442c450741fddc73ff8e297b7d73c316fe23c99
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-05-12 14:54:31 +0200

    a

---
+++ sql/02 data/02 eigendata.sql
@@ -0,0 +1,10 @@
+INSERT INTO `eigen data` (`id`, `opera`, `klok`, `huisdier`, `vs`, `blijheid/status`) 
+VALUES (NULL, 'gebruik ik veel', '1448', 'niet', 'veel gebruikt de laatste tijd', '7');
+
+SELECT opera FROM 'eigen data';
+
+SELECT * FROM 'eigen data';
+
+SELECT vs,opera FROM 'eigen data';
+
+SELECT klok,huisdier FROM 'eigen data';
