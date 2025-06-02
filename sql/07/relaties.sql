d41ad8c01fb16339161923f9b8c2a559e2cbc27c
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-06-02 14:23:47 +0200

    Update relaties.sql

--- sql/07/relaties.sql
+++ sql/07/relaties.sql
@@ -4,3 +4,19 @@ insert into ingredient (id,naam) values
 
 insert into voedselProduct (hoofdIngredientId,naam) values 
 (1,'pindakaas');
+
+
+
+ ALTER TABLE voedselProduct
+ ADD CONSTRAINT FK_voedselProduct_ingredient
+ FOREIGN KEY (hoofdIngredientId) REFERENCES  
+ TABEL2(id);
+
+ insert into voedselProduct (hoofdIngredientId,naam) values 
+(2,'honing');
+
+insert into voedselProduct (hoofdIngredientId,naam) values 
+(3,'bosbessenkwark');
+
+insert into ingredient (id,naam) values 
+(3,'bosbessen');
